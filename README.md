50685d59539a9fc0713bdbb01a4803471f3058cc
Robert Middleton <robert.middleton@rm5248.com>
2024-05-18 22:57:43 -0400

    updated licensing information

---
+++ README.md
@@ -0,0 +1,7 @@
+# LCC Tortoise
+
+Code for LCC Tortoise.  Based off of Zephyr RTOS.
+
+## License
+
+GPL v2 ONLY
--- src/main.c
+++ src/main.c
@@ -1,7 +1,5 @@
 /*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: GPL-2.0
  */
 
 #include <stdio.h>
