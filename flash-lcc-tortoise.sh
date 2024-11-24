e9b15eea67676d68b40213d9f2f66167777ca4d3
Robert Middleton <robert.middleton@rm5248.com>
2024-11-24 17:58:44 -0500

    Added script to run the lcc tortoise

---
+++ flash-lcc-tortoise.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+STM32_CLI=~/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI
+
+# Set nBOOT0 to 0 to allow us to start the bootloader
+"$STM32_CLI" -c port=swd -ob nBOOT_SEL=0
+# Flash the main code
+"$STM32_CLI" -c port=swd -w build/lcc-tortoise/zephyr/lcc-tortoise8.signed.bin 0x08008000
+# Flash the bootloader
+"$STM32_CLI" -c port=swd -w build/mcuboot/zephyr/zephyr.bin 0x08000000
+# Start it up
+#"$STM32_CLI" -c port=swd --start 0x08000000
