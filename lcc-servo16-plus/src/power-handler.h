ec66a3208898f8303c5b3d8b881cb1700e13b78c
Robert Middleton <robert.middleton@rm5248.com>
2025-11-29 19:33:42 -0500

    copied over power handler code

---
+++ lcc-servo16-plus/src/power-handler.h
@@ -0,0 +1,27 @@
+/*
+ * power-handler.h
+ *
+ *  Created on: Aug 11, 2024
+ *      Author: robert
+ */
+
+#ifndef LCC_TORTOISE_SRC_POWER_HANDLER_H_
+#define LCC_TORTOISE_SRC_POWER_HANDLER_H_
+
+struct adc_readings{
+	int volts_mv;
+	int vin_mv;
+	int current;
+};
+
+void powerhandle_init();
+
+/**
+ * When settings change, call this function to configure the power handling.
+ * This is needed so that we don't write to the flash if we don't need to.
+ */
+void powerhandle_check_if_save_required();
+
+void powerhandle_current_volts_mv(struct adc_readings*);
+
+#endif /* LCC_TORTOISE_SRC_POWER_HANDLER_H_ */
